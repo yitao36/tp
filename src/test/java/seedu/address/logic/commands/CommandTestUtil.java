ad668cf1355634a2c7fc087c6e94362ada19b5f3
3seastacks <194687609+3seastacks@users.noreply.github.com>
2025-10-25 22:42:19 +0800

    Fix conflict

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -20,6 +20,7 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.Event;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.EditPersonDescriptorBuilder;
@@ -165,4 +166,18 @@ public class CommandTestUtil {
         Model emptyModel = new ModelManager();
         command.execute(emptyModel);
     }
+
+    /**
+     * Updates {@code model}'s filtered list to show only the event at the given {@code targetIndex} in the
+     * {@code model}'s address book.
+     */
+    public static void showEventAtIndex(Model model, Index targetIndex) {
+        assertTrue(targetIndex.getZeroBased() < model.getFilteredEventList().size());
+
+        Event event = model.getFilteredEventList().get(targetIndex.getZeroBased());
+        model.updateFilteredEventList(val -> val.equals(event));
+
+        assertEquals(1, model.getFilteredEventList().size());
+    }
+
 }
